ac82363f94e360c50afb3d561032a54d8163400c
HarshitBisen <65802819+HarshitBisen@users.noreply.github.com>
2022-06-06 17:46:49 +0530

    Create MergeTwoSortedArrays.py

---
+++ MergeTwoSortedArrays.py
@@ -0,0 +1,11 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+def ninjaAndSortedArrays(arr1,arr2,m,n):
+    # Write your code here.
+    for i in range(n):
+        arr1[i+m]=arr2[i]
+    arr1.sort()
+    return arr1

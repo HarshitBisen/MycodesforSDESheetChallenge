9da19738449fe870d416c7ad02e88e5d495cbad3
HarshitBisen <65802819+HarshitBisen@users.noreply.github.com>
2022-06-10 08:59:35 +0530

    Create MergeTwoSortedLinkedList.py

---
+++ MergeTwoSortedLinkedList.py
@@ -0,0 +1,81 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+import sys
+from sys import stdin
+
+# Following is the linked list node structure:
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+      
+def sortTwoLists(first, second):
+    dummy=Node(2)
+    tail=dummy
+    
+    while first and second:
+        if first.data<second.data:
+            tail.next=first
+            first=first.next
+        else:
+            tail.next=second
+            second=second.next
+        tail=tail.next
+    if first:
+        tail.next=first
+    else:
+        tail.next=second
+    return dummy.next
+
+
+
+
+
+
+
+
+
+def ll(arr):
+    
+    if len(arr)==0:
+        return None
+    
+    head = Node(arr[0])
+    last = head
+    
+    for data in arr[1:]:
+        
+        last.next = Node(data)
+        last = last.next
+        
+    return head
+
+def printll(head):
+    
+    while head:
+        
+        print(head.data, end=' ')
+        
+        head = head.next
+        
+    print(-1)
+
+    
+
+t = int(sys.stdin.readline().strip())
+
+for i in range(t):
+    
+    arr1=list(map(int, sys.stdin.readline().strip().split(" ")))
+    arr2=list(map(int, sys.stdin.readline().strip().split(" ")))
+    
+    l1 = ll(arr1[:-1])
+    l2 = ll(arr2[:-1])
+    
+    l = sortTwoLists(l1, l2)
+    
+    printll(l)
+
